7acadbb93c002a3756304abf8c3f168820f1a2cb
Berrio2411 <santiagoberrio01@gmail.com>
2020-09-15 08:14:59 -0500

    taller while

--- Talleres/taller2.py
+++ Talleres/taller2.py
@@ -20,6 +20,33 @@ while(numeroX>numeroY):
     numeroY=int(input(preguntaNumeroY))  
 
 #--Ejericio3--
+preguntaNumeroA = "Ingrese un número entero por favor : "
+preguntaNumeroB = "Ingrese un número mayor al ultimo ingresado : "
+mensajeInformacion = "{} es mayor a {} por eso sigue el juego"
+mensajeEntrada = "Ingresaste un numero mayor, solo acabaras cuando ingreses uno menor al ultimo numero ingresado"
+
+primerNumero =  int (input (preguntaNumeroA))
+segundoNumero =  int (input (preguntaNumeroB))
+
+while (segundoNumero > primerNumero) :
+  print (mensajeInformacion.format (segundoNumero, primerNumero))
+  primerNumero = segundoNumero
+  print (mensajeEntrada)
+  segundoNumero =  int (input (preguntaNumeroB))
+
+
+#Ejercicio4
+total=0
+monto=float(input("Monto de una venta: $"))
+while monto!=0:
+    if monto<0:
+        print("Monto no válido.")
+    else:
+        total+=monto
+    monto=float(input("Monto de una venta: $"))
+if total>1000:
+    total-=total*0.1
+print("Monto total a pagar: $", total) 
 
 
 
